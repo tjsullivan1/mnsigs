e0bacdd656a7e44de8558ad9c27588b3f4f67397
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-07 22:41:52 -0500

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,38 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact" />
+    <form method="post" action="#">
+      <label>
+        Name
+        <input type="text" name="name" id="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" id="email" />
+      </label>
+      <label>
+        Phone
+        <input type="phone" name="phone" id="phone" />
+      </label>
+      <label>
+        Subject
+        <input type="text" name="subject" id="subject" />
+      </label>
+      <label>
+        Message
+        <textarea name="message" id="message" rows="5" />
+      </label>
+      <button type="submit">Send</button>
+      <input type="reset" value="Clear" />
+    </form>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ContactPage
