e92fabcf3facc8c544aee88677c06a1a93914b1f
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-22 20:11:03 -0500

    navbar x2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,18 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Welcome to the home of the Greater Minnesota Alumni Chapter</h1>
-    <p>This site will host content about us, as well as applications built to support our operations.</p>
+    
     <div class="navbar">
       <ul>
-      <li><Link to="/about/">About us</Link></li>
-      <li><Link to="/bylaws/">Our bylaws</Link></li>
-      <li><Link to="/contact/">Contact us</Link></li>
+        <li><Link to="/about/">About us</Link></li>
+        <li><Link to="/bylaws/">Our bylaws</Link></li>
+        <li><Link to="/contact/">Contact us</Link></li>
      </ul>
     </div>
+
+    <h1>Welcome to the home of the Greater Minnesota Alumni Chapter</h1>
+    <p>This site will host content about us, as well as applications built to support our operations.</p>
+
   </Layout>
 )
 
