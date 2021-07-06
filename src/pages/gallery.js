4e71a63c8388e9f1d4b1cc8ae33c18c5e1cbd626
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-07-05 23:06:50 -0500

    fixing image tags

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -9,19 +9,19 @@ const GalleryPage = () => (
         <SEO title="Gallery" />
         <div class="gallery">
         <a target="_blank" href="../imgages/gallery/dj_harrelson_phillips.jpg">
-            <img src="../imgages/gallery/dj_harrelson_phillips.jpg" alt="Dj, Woody, Rep. Phillips" width="600" height="400">
+            <img src="../imgages/gallery/dj_harrelson_phillips.jpg" alt="Dj, Woody, Rep. Phillips" width="600" height="400" />
         </a>
         <div class="desc">Brother DJ Gries (Greater Minnesota Alumni Chapter President, 2021-2023) with Significant Sigs Woody Harrelson and Rep. Dean Phillips</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="../imgages/gallery/gc_2021.jpg">
-            <img src="../imgages/gallery/gc_2021.jpg" alt="Brothers at the 83rd Grand Chapter in Phoenix" width="600" height="400">
+            <img src="../imgages/gallery/gc_2021.jpg" alt="Brothers at the 83rd Grand Chapter in Phoenix" width="600" height="400" />
         </a>
         <div class="desc">Brothers at the 83rd Grand Chapter in Phoenix</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="../imgages/gallery/province_2020.jpg">
-            <img src="../imgages/gallery/province_2020.jpg" alt="Brothers after the 2020 North Central Province Conference" width="600" height="400">
+            <img src="../imgages/gallery/province_2020.jpg" alt="Brothers after the 2020 North Central Province Conference" width="600" height="400" />
         </a>
         <div class="desc">Brothers after the 2020 North Central Province Conference</div>
         </div>
