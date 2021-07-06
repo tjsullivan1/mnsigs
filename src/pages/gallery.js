a217610ead997711ed170ecbc6e7d72c44fc60a4
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-07-06 14:53:28 -0500

    moving away from static image calls

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -10,40 +10,20 @@ const GalleryPage = () => (
         <SEO title="Gallery" />
         <div class="gallery">
         <a target="_blank" href="https://samnsigs001.blob.core.windows.net/media/photos/dj_harrelson_philips.jpg">
-            <StaticImage
-            src="https://samnsigs001.blob.core.windows.net/media/photos/dj_harrelson_philips.jpg"
-            width={600}
-            quality={400}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="Dj, Woody, Rep. Phillips"
-            style={{ marginBottom: `1.45rem` }}
-            />
+            <img src="https://samnsigs001.blob.core.windows.net/media/photos/dj_harrelson_philips.jpg" alt="Dj, Woody, Rep. Phillips" width="600" height="400" />
         </a>
         <div class="desc">Brother DJ Gries (Greater Minnesota Alumni Chapter President, 2021-2023) with Significant Sigs Woody Harrelson and Rep. Dean Phillips</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="https://samnsigs001.blob.core.windows.net/media/photos/gc_2021.jpg">
-        <StaticImage
-            src="https://samnsigs001.blob.core.windows.net/media/photos/gc_2021.jpg"
-            width={600}
-            quality={400}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="Brothers at the 83rd Grand Chapter in Phoenix"
-            style={{ marginBottom: `1.45rem` }}
-            />
+            <img src="https://samnsigs001.blob.core.windows.net/media/photos/gc_2021.jpg" alt="Brothers at Grand Chapter 2021" width="600" height="400" />
+
         </a>
         <div class="desc">Brothers at the 83rd Grand Chapter in Phoenix</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="https://samnsigs001.blob.core.windows.net/media/photos/province-2020.jpg">
-        <StaticImage
-            src="https://samnsigs001.blob.core.windows.net/media/photos/province-2020.jpg"
-            width={600}
-            quality={400}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="Brothers after the 2020 North Central Province Conference"
-            style={{ marginBottom: `1.45rem` }}
-            />
+            <img src="https://samnsigs001.blob.core.windows.net/media/photos/province-2020.jpg" alt="Brothers after Province Conference 2020" width="600" height="400" />
         </a>
         <div class="desc">Brothers after the 2020 North Central Province Conference</div>
         </div>
