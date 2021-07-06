54388fc3049eacf0a3ee5593c7d41b4639274856
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-07-05 23:32:12 -0500

    trying with static images

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,19 +10,40 @@ const GalleryPage = () => (
         <SEO title="Gallery" />
         <div class="gallery">
         <a target="_blank" href="../images/dj_harrelson_phillips.jpg">
-            <img src="../images/dj_harrelson_phillips.jpg" alt="Dj, Woody, Rep. Phillips" width="600" height="400" />
+            <StaticImage
+            src="../images/dj_harrelson_phillips.jpg"
+            width={600}
+            quality={400}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Dj, Woody, Rep. Phillips"
+            style={{ marginBottom: `1.45rem` }}
+            />
         </a>
         <div class="desc">Brother DJ Gries (Greater Minnesota Alumni Chapter President, 2021-2023) with Significant Sigs Woody Harrelson and Rep. Dean Phillips</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="../images/gc_2021.jpg">
-            <img src="../images/gc_2021.jpg" alt="Brothers at the 83rd Grand Chapter in Phoenix" width="600" height="400" />
+        <StaticImage
+            src="../images/gc_2021.jpg"
+            width={600}
+            quality={400}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Brothers at the 83rd Grand Chapter in Phoenix"
+            style={{ marginBottom: `1.45rem` }}
+            />
         </a>
         <div class="desc">Brothers at the 83rd Grand Chapter in Phoenix</div>
         </div>
         <div class="gallery">
         <a target="_blank" href="../images/province_2020.jpg">
-            <img src="../images/province_2020.jpg" alt="Brothers after the 2020 North Central Province Conference" width="600" height="400" />
+        <StaticImage
+            src="../images/province_2020.jpg"
+            width={600}
+            quality={400}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Brothers after the 2020 North Central Province Conference"
+            style={{ marginBottom: `1.45rem` }}
+            />
         </a>
         <div class="desc">Brothers after the 2020 North Central Province Conference</div>
         </div>
@@ -30,3 +52,5 @@ const GalleryPage = () => (
 )
 
 export default GalleryPage
+
+
