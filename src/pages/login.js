5bb2b860beb8807b50e24534fb572917b6f2bc38
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-08-10 22:33:54 -0500

    Trying to add authentication options

---
+++ src/pages/login.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="login" />
+
+    <h1 class="center">Login</h1>
+    <ul>
+        <li><Link to="/.auth/login/aad" class="overrideLiAInteraction">Login with Email</Link></li>
+        <li><Link to="/.auth/login/github" class="overrideLiAInteraction">Login with GitHub</Link></li>
+        <li><Link to="/.auth/login/twitter" class="overrideLiAInteraction">Login with Twitter</Link></li>
+    </ul>
+
+  </Layout>
+)
+
+export default IndexPage
