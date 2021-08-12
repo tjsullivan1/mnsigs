94159917641a7693d3faa4c9856db71b16cca78c
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-08-11 22:16:53 -0500

    url try #3

--- src/pages/members/minutes.js
+++ src/pages/members/minutes.js
@@ -7,7 +7,8 @@ import SEO from "../../components/seo"
 const MinutesPage = () => (
   <Layout>
     <SEO title="Minutes" />
-    <a href="{process.env.GATSBY_URL_1}" class="overrideLiAInteraction">Test File</a>
+    <a href="{process.env.GATSBY_URL_1}" class="overrideLiAInteraction">Test File 3</a>
+    <p>Hello, {process.env.GATSBY_URL_1}</p>
   </Layout>
 )
 
