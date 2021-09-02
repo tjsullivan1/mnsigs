77e529f284eec007bb277a3addb0055f06135320
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-09-01 23:52:26 -0500

    Updated file names to be upper case in react components

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,8 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import Footer from "./footer"
+import Header from "./Header"
+import Footer from "./Footer"
 import "./../styles/layout.css"
 
 const Layout = ({ children }) => {
