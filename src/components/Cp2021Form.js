f604f807bb2c0d8aa39754441c0102e96bbe2fce
Tim Sullivan <timothyj.sullivan1@github.com>
2021-10-14 21:33:58 -0500

    removing mailchimp stuff from cp2021form component

--- src/components/Cp2021Form.js
+++ src/components/Cp2021Form.js
@@ -3,28 +3,6 @@ import React, { useState } from "react"
 import Button from "../components/Button"
 
 const TicketForm = () => {
-  // const [email, setEmail] = useState()
-  // const [message, setMessage] = useState()
-  // const [disabled, setDisabled] = useState(false)
-
-  // const handleSubmit = async event => {
-  //   event.preventDefault()
-  //   setDisabled(true)
-  //   setMessage("Sending...")
-  //   const response = await addToMailchimp(email)
-  //   if (response.result === "error") {
-  //     if (response.msg.toLowerCase().includes("already subscribed")) {
-  //       setMessage("You're already on to the list!")
-  //     } else {
-  //       setMessage("Some error occured while subscribing you to the list.")
-  //     }
-  //     setDisabled(false)
-  //   } else {
-  //     setMessage(
-  //       "Thanks! Please check your e-mail and click the confirmation link."
-  //     )
-  //   }
-  // }
 
   return (
     <div className="container py-12 lg:py-16">
