cfb0359a2e1ad8d0295ed27912fd9377d914523d
Tim Sullivan <timothyj.sullivan1@github.com>
2021-10-14 20:54:25 -0500

    initial christmas party pages

---
+++ src/pages/cp2021/cancel.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../../components/Layout"
+import Seo from "../../components/SEO"
+
+const cp2021Cancel = () => (
+  <Layout>
+    <Seo title="Cancelled Registration: Christmas Party 2021" />
+    <h1>Cancelled Registration: Christmas Party 2021</h1>
+    <p>We're sorry you decided that the Christmas Party 2021 wasn't for you and your guests. If you got this page in error please reach out to Tim and Derrek.</p>
+  </Layout>
+)
+
+export default cp2021Cancel
---
+++ src/pages/cp2021/success.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../../components/Layout"
+import Seo from "../../components/SEO"
+
+const cp2021Success = () => (
+  <Layout>
+    <Seo title="Successful Registration: Christmas Party 2021" />
+    <h1>Successful Registration: Christmas Party 2021</h1>
+    <p>We're so looking forward to seeing you and your guests at the event! Please expect us to be in touch.</p>
+  </Layout>
+)
+
+export default cp2021Success
