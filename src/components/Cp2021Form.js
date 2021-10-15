dcd329f286499d863eef780b21f6f3b229b45864
Tim Sullivan <timothyj.sullivan1@github.com>
2021-10-14 21:28:05 -0500

    removing mailchimp stuf form cp2021form component

--- src/components/Cp2021Form.js
+++ src/components/Cp2021Form.js
@@ -46,11 +46,6 @@ const TicketForm = () => {
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
     </form>
-      <div
-        className={classNames("w-full pt-4 text-sm", { "opacity-0": !message })}
-      >
-        {message}
-      </div>
     </div>
   )
 }
