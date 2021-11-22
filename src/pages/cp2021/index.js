521a3d477fb3afdb23fec7403f4151d464d4f37b
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-11-21 21:21:35 -0600

    updated christmas party details

--- src/pages/cp2021/index.js
+++ src/pages/cp2021/index.js
@@ -11,6 +11,13 @@ const cp2021IndexPage = () => (
     <Seo title="Christmas Party 2021!" />
 
     <h1 class="center">Get your tickets for the party today!</h1><br/>
+    <h2>Details</h2>
+    <ul>
+      <li>Where: Derrek VanLith's Apartment Building Party Room (200 University Ave SE, Minneapolis, MN)</li>
+      <li>When: December 11, 2021 5:30-10pm</li>
+      <li>What: A chance to enjoy some brotherhood with some guests. We will have beer, wine, selzers, and hearty appetizers (all included in ticket price)</li>
+      </ul>
+    <h2>Purchase</h2>
     <p>Please use the form below to purchase your tickets. Please select whether you are a dues paying member, brother, or guest. This is a rudimentary form, if you are a brother bringing a guest, please select your status and then change quantity to 2.</p><br/>
     <TicketForm />
 
