e6a4935ded94a47ddfe75b35e347493adc329a15
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-06-16 23:28:15 -0500

    removed old form from contact page, we just want people to sign up for the mailing list

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import MailchimpForm from "../components/MailchimpForm"
 
 import Layout from "../components/Layout"
@@ -10,27 +9,7 @@ const ContactPage = () => (
     <Seo title="Contact" />
     <h1 class="center">Subscribe to our mailing list!</h1><br />
       <MailchimpForm/>
-    <h1 class="center">Questions or Comments? Let us know!</h1><br />
-    <h3>Send us a message</h3>
-    <form method="post" action="https://mnsigs-contact.azurewebsites.net/api/contact?code=DC287c4iya8GoR0KlNNbA/qa7iL6Qhv39ZN57NqtV7rGMKEhHoD/vA==">
-        <label>Name <strong style={{ color:'red' }}>*</strong></label>
-        <input type="text" name="name" id="name" />
 
-        <label>Email <strong style={{ color:'red' }}>*</strong></label>
-        <input type="email" name="email" id="email" />
-
-        <label>Phone</label>
-        <input type="phone" name="phone" id="phone" />
-
-        <label>Subject <strong style={{ color:'red' }}>*</strong></label>
-        <input type="text" name="subject" id="subject" />
-
-        <label>Message <strong style={{ color:'red' }}>*</strong></label>
-        <textarea name="message" id="message" rows="5" /><br/>
-
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-    </form>
   </Layout>
 )
 
