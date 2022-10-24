b2186e2a9947b8a315582993f5bc0accc0530f80
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-10-24 11:21:48 -0500

    updated info on pricing

--- src/pages/cp2022/index.js
+++ src/pages/cp2022/index.js
@@ -18,8 +18,9 @@ const cp2022IndexPage = () => (
     <h2>Purchase</h2>
     <p>Please use the form below to purchase your tickets. Please select whether you are a dues paying member, brother, or guest. This is a rudimentary form, if you are a brother bringing a guest, please select your status and then change quantity to 2.</p><br/>
     <TicketForm />
-
-
+    <h2>On Pricing</h2>
+    <p>We have heard some feedback on the pricing for this event, and in the spirit of transparency we do want to clarify several things. This event is intended to deliver you a significant amount of value, but we do charge a premium to use this as a fundraiser for chapter operations and long-term growth. You should expect to be seeing about $25-$30 worth of value on your ticket. We do not want the pricing to be an impediment to any brothers attanedance, so if this is the case for you, please reach out to one of the members of the executive committee.</p>
+    <p>Regarding dues-paying vs. non-dues paying, we've strategically discounted this event to be $10 cheaper per person attending on a dues-paying member's bill. The rationale here is to incentivize the paying of dues to the Alumni Chapter. This $10 discount represents half of your dues annaully, and if you bring a guest, your dues pay for themselves! If you haven't paid your dues yet, but would like to see this discount - please reach out to Tim!</p>
   </Layout>
 )
 
